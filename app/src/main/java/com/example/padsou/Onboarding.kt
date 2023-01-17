9cab4a775179641640db41dd019e8ccd6967fc43
thomaspeissel <peisselt@iut-acy.univ-smb.fr>
2023-01-17 10:52:29 +0100

    Onboarding

--- app/src/main/java/com/example/padsou/Onboarding.kt
+++ app/src/main/java/com/example/padsou/Onboarding.kt
@@ -3,18 +3,25 @@ package com.example.padsou
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.padsou.ui.theme.*
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.HorizontalPagerIndicator
+import com.google.accompanist.pager.rememberPagerState
 
 class Onboarding : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,9 +38,11 @@ class Onboarding : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalPagerApi::class)
 @Preview
 @Composable
 fun MainOnboarding() {
+    val pagerState = rememberPagerState()
     Column(
         modifier = Modifier
             .background(PadsouPurple)
@@ -66,13 +75,39 @@ fun MainOnboarding() {
             }
             Row() {
                 Column() {
-                    Box(
+                    HorizontalPagerIndicator(
+                        pagerState = pagerState,
+                        activeColor = Color.White,
+                        inactiveColor = PadsouLightGrey,
+                        indicatorWidth = 25.dp,
+                        indicatorHeight = 5.dp,
+                        spacing = 10.dp,
                         modifier = Modifier
-                            .fillMaxWidth(fraction = 1f)
-                            .fillMaxHeight(fraction = 0.5f)
-                            .clip(shape = RoundedCornerShape(25.dp))
-                            .background(Color.White)
+                            .padding(bottom = 22.dp),
                     )
+                    HorizontalPager(
+                        itemSpacing = 25.dp,
+                        count = 3,
+                        state = pagerState
+                    ) { _ ->
+                        Box(
+                            contentAlignment = Alignment.Center,
+                            modifier = Modifier
+                                .width(250.dp)
+                                .height(250.dp)
+                                .clip(shape = RoundedCornerShape(25.dp))
+                                .background(Color.White)
+                        ){
+                            Image(
+                                painter = painterResource(id = R.drawable.menus),
+                                contentDescription = "Menus Item",
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .fillMaxHeight()
+                                    .padding(10.dp)
+                            )
+                        }
+                    }
                     Text(
                         text = "Accède aux 500 bons plans qu’on te propose chaque mois",
                         color = Color.White,
