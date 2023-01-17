afd5ebc6baef996d566fd3d5312cf3c80165808f
thomaspeissel <peisselt@iut-acy.univ-smb.fr>
2023-01-17 08:49:31 +0100

    Homes

--- app/src/main/java/com/example/padsou/ui/theme/Type.kt
+++ app/src/main/java/com/example/padsou/ui/theme/Type.kt
@@ -1,6 +1,5 @@
 package com.example.padsou.ui.theme
 
-import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
